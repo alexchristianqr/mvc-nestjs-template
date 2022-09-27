e16bee3f7bffd102f76cf39bf48f7a6029309a8b
Alex Christian <alexchristianqr@gmail.com>
2022-09-27 16:55:59 -0500

    [updated] config prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [PrismaModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => {
+        return {
+          prismaOptions: {
+            log: [configService.get('log')],
+            datasources: {
+              db: {
+                url: configService.get('DATABASE_URL'),
+              },
+            },
+          },
+          explicitConnect: configService.get('explicit'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
